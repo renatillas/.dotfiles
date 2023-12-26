b9daee4a8df9ebd457bf2406e0849475cba88fe2
Renata Amutio Herrero <renata-amutio@Renatas-MacBook-Pro.local>
2023-12-26 11:35:17 +0100

    feat(nvim): pluggins added

---
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -0,0 +1,23 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = { "hrsh7th/cmp-emoji" },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local cmp = require("cmp")
+    return {
+      window = {
+        documentation = cmp.config.window.bordered(),
+        completion = cmp.config.window.bordered(),
+      },
+      sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } })),
+      mapping = cmp.mapping.preset.insert({
+        ["<C-n>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-p>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
+      })
+    }
+  end,
+}
