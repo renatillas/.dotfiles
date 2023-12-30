39213be55ba669dfbcab4a9f334bfc7418a8a5b8
Renata Amutio Herrero <renata-amutio@Renatas-MacBook-Pro.local>
2023-12-30 22:09:36 +0100

    fix(nvim): fixed oil config

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,14 +1,16 @@
 return {
   "stevearc/oil.nvim",
-  opts = {},
+  opts = {
+    view_options = {
+      show_hidden = true,
+    },
+    delete_to_trash = true,
+    skip_confirm_for_simple_edits = true,
+  },
   -- Optional dependencies
   dependencies = { "nvim-tree/nvim-web-devicons" },
   keys = {
     { "-", "<cmd>Oil<cr>", desc = "Open parent directory" },
     { "_", "<cmd>Oil --float<cr>", desc = "Open parent directory" },
   },
-  view_options = {
-    show_hidden = true,
-  },
-  delete_to_trash = true,
 }
