0dabd56fc9a7c68be92caae462ca0340860548cf
Renata Amutio Herrero <renata-amutio@Renatas-MacBook-Pro.local>
2024-01-04 01:15:22 +0100

    feat(fish): move from zsh to fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,8 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+# To add homebrew to path
+fish_add_path /opt/homebrew/bin
+
+# To init zoxide 
+zoxide init fish | source
