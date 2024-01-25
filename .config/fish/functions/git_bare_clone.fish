38d8cb69cc86c924436084079ea4a6f0e78efe66
Renata Amutio Herrero <renata.amutio@gmail.com>
2024-01-26 00:27:30 +0100

    feat(fish): Improved git bare clone script

--- .config/fish/functions/git_bare_clone.fish
+++ .config/fish/functions/git_bare_clone.fish
@@ -1,7 +1,7 @@
 function git_bare_clone --argument repo_url
     # Check if the number of arguments is correct
     if test (count $argv) -ne 1
-        echo "Usage: git_clone_and_config <repository_url>"
+        echo "[ERROR(git_bare_clone)]: Usage: git_clone_and_config <repository_url>" >&2
         return 1
     end
 
@@ -10,7 +10,7 @@ function git_bare_clone --argument repo_url
 
     # Clone the repository
     if not git clone --bare $repo_url $repo_name
-        echo "Failed to clone repository: $repo_url"
+        echo "[ERROR(git_bare_clone)]: Failed to clone repository: $repo_url" >&2
         return 1
     end
 
@@ -21,9 +21,20 @@ function git_bare_clone --argument repo_url
     if not git config --get remote.origin.fetch
         # If not present, run the git config command
         if not git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
-            echo "Failed to run git config command"
+            echo "[ERROR(git_bare_clone)]: Failed to run git config command" >&2
             return 1
         end
+    end
+
+    # Delete all local branches except master to mitigate bare copying all heads
+    if not git branch | grep -v master | xargs git branch -D
+        echo "[ERROR(git_bare_clone)]: Failed to delete all local branches" >&2
+        return 1
+    end
+
+    if not git fetch
+        echo "[ERROR(git_bare_clone)]: Failed to fetch from remote" >&2
+        return 1
     end
 
     # Change back to the original directory
