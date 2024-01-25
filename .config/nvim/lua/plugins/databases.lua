e33e75ceb80ff09bb9d87bba5bc38e00152ae326
Renata Amutio Herrero <renata.amutio@gmail.com>
2024-01-26 00:38:15 +0100

    feat(nvim): database plugins improved with lsp

--- .config/nvim/lua/plugins/databases.lua
+++ .config/nvim/lua/plugins/databases.lua
@@ -1,25 +1,42 @@
 return {
-  "kristijanhusak/vim-dadbod-ui",
-  dependencies = {
-    {
-      "tpope/vim-dadbod",
-      lazy = true,
+  {
+    "kristijanhusak/vim-dadbod-ui",
+    dependencies = {
+      {
+        "tpope/vim-dadbod",
+        lazy = true,
+      },
+      {
+        "kristijanhusak/vim-dadbod-completion",
+        ft = { "sql", "mysql", "plsql" },
+        lazy = true,
+      },
     },
-    {
-      "kristijanhusak/vim-dadbod-completion",
-      ft = { "sql", "mysql", "plsql" },
-      lazy = true,
+    cmd = {
+      "DBUI",
+      "DBUIToggle",
+      "DBUIAddConnection",
+      "DBUIFindBuffer",
     },
+    init = function()
+      -- Your DBUI configuration
+      vim.g.db_ui_use_nerd_fonts = 1
+      vim.g.dbs = { dev = "postgresql://fever_user:fever_password@localhost:54321/fever" }
+    end,
   },
-  cmd = {
-    "DBUI",
-    "DBUIToggle",
-    "DBUIAddConnection",
-    "DBUIFindBuffer",
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      {
+        "kristijanhusak/vim-dadbod-completion",
+      },
+    },
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, {
+        { name = "vim-dadbod-completion" },
+      }))
+    end,
   },
-  init = function()
-    -- Your DBUI configuration
-    vim.g.db_ui_use_nerd_fonts = 1
-    vim.g.dbs = { dev = "postgresql://fever_user:fever_password@localhost:54321/fever" }
-  end,
 }
