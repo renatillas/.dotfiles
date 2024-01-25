21d3cfcc3719f12f9b0e8aa845c2737bc2f49eb3
Renata Amutio Herrero <renata.amutio@gmail.com>
2024-01-26 00:40:11 +0100

    feat(nvim): copiot using official plugin

--- .config/nvim/lua/plugins/ia.lua
+++ .config/nvim/lua/plugins/ia.lua
@@ -1,9 +1,4 @@
 return {
   { "David-Kunz/gen.nvim" },
-  {
-    "zbirenbaum/copilot.lua",
-    opts = {
-      copilot_node_command = vim.fn.expand("$HOME") .. "/.local/share/nvm/v20.10.0/bin/node",
-    },
-  },
+  { "github/copilot.vim" },
 }
